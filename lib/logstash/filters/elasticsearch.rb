f02ff62a7966cdc8c37fe481eef9ca6852c6d8e1
Suyog Rao <suyog.rao@gmail.com>
2014-11-06 16:31:45 -0800

    Added inline code asciidoc formatting

--- lib/logstash/filters/elasticsearch.rb
+++ lib/logstash/filters/elasticsearch.rb
@@ -7,10 +7,10 @@ require "logstash/util/fieldreference"
 # into the current event.  Below is a complete example of how this filter might
 # be used.  Whenever logstash receives an "end" event, it uses this elasticsearch
 # filter to find the matching "start" event based on some operation identifier.
-# Then it copies the @timestamp field from the "start" event into a new field on
+# Then it copies the `@timestamp` field from the "start" event into a new field on
 # the "end" event.  Finally, using a combination of the "date" filter and the
 # "ruby" filter, we calculate the time duration in hours between the two events.
-#
+# [source,ruby]
 #       if [type] == "end" {
 #          elasticsearch {
 #             hosts => ["es-server"]
@@ -38,7 +38,7 @@ class LogStash::Filters::Elasticsearch < LogStash::Filters::Base
   # Elasticsearch query string
   config :query, :validate => :string
 
-  # Comma-delimited list of <field>:<direction> pairs that define the sort order
+  # Comma-delimited list of `<field>:<direction>` pairs that define the sort order
   config :sort, :validate => :string, :default => "@timestamp:desc"
 
   # Hash of fields to copy from old event (found via elasticsearch) into new event
