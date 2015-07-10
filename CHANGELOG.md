a2451d305e3592e187136c54b8221fb3a447d8e7
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-07-10 12:10:53 +0200

    version 0.16 bump, plus changelog update

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.6
+
+- removed require statement for a file that is no longer present in logstash-core.
--- spec/filters/elasticsearch_spec.rb
+++ spec/filters/elasticsearch_spec.rb
@@ -2,8 +2,9 @@
 
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/plugin"
+require "logstash/filters/elasticsearch"
 
-describe "filter/elasticsearch" do
+describe LogStash::Filters::Elasticsearch do
 
   it "should register" do
     plugin = LogStash::Plugin.lookup("filter", "elasticsearch").new({})
