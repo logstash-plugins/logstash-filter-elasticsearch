7b4eeda7956fd72cc9a061a747fcf8eaebfcd10d
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-07-10 12:08:26 +0200

    remove require util/filereference as it's no longer in LS
    Fixes #19

--- spec/filters/elasticsearch_spec.rb
+++ spec/filters/elasticsearch_spec.rb
@@ -6,7 +6,7 @@ require "logstash/plugin"
 describe "filter/elasticsearch" do
 
   it "should register" do
-    plugin = LogStash::Plugin.lookup("filter", "elasticsearch").new
+    plugin = LogStash::Plugin.lookup("filter", "elasticsearch").new({})
     expect {plugin.register}.to_not raise_error
   end
 end
