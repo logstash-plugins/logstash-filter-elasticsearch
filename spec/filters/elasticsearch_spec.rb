47c2106f92d1c1dfecbdb383a75127c54949301a
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-07-10 12:01:57 +0200

    add a simple test to handle plugin registration so referencies could be check

--- spec/filters/elasticsearch_spec.rb
+++ spec/filters/elasticsearch_spec.rb
@@ -1,1 +1,12 @@
+# encoding: utf-8
+
 require "logstash/devutils/rspec/spec_helper"
+require "logstash/plugin"
+
+describe "filter/elasticsearch" do
+
+  it "should register" do
+    plugin = LogStash::Plugin.lookup("filter", "elasticsearch").new
+    expect {plugin.register}.to_not raise_error
+  end
+end
