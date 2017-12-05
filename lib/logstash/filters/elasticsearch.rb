827563ab563b92549a7f34c5acbe946d095ee23e
pemontto <pemontto@gmail.com>
2017-12-05 14:34:26 -0500

    Renamed to docinfo_fields and update docs

--- lib/logstash/filters/elasticsearch.rb
+++ lib/logstash/filters/elasticsearch.rb
@@ -88,7 +88,7 @@ class LogStash::Filters::Elasticsearch < LogStash::Filters::Base
   # List of elasticsearch hosts to use for querying.
   config :hosts, :validate => :array,  :default => [ "localhost:9200" ]
 
-  # Comma-delimited list of index names to search; use `_all` or empty string to perform the operation on all indices
+  # Comma-delimited list of index names to search; use `_all` or empty string to perform the operation on all indices.
   # Field substitution (e.g. `index-name-%{date_field}`) is available
   config :index, :validate => :string, :default => ""
 
@@ -103,11 +103,11 @@ class LogStash::Filters::Elasticsearch < LogStash::Filters::Base
   # Comma-delimited list of `<field>:<direction>` pairs that define the sort order
   config :sort, :validate => :string, :default => "@timestamp:desc"
 
-  # Array of fields to copy from old event (found via elasticsearch) into new event
-  config :fields, :validate => :array, :default => {}
+  # Hash of fields to copy from old event (found via elasticsearch) into new event
+  config :fields, :validate => :hash, :default => {}
 
-  # Array of meta-fields to copy from old event (found via elasticsearch) into new event
-  config :meta_fields, :validate => :array, :default => {}
+  # Hash of document metadata fields copy from old event (found via elasticsearch) into new event
+  config :docinfo_fields, :validate => :hash, :default => {}
 
   # Basic Auth - username
   config :user, :validate => :string
@@ -173,7 +173,7 @@ class LogStash::Filters::Elasticsearch < LogStash::Filters::Base
           event.set(new_key, set.count > 1 ? set : set.first)
         end
       end
-      @meta_fields.each do |old_key, new_key|
+      @docinfo_fields.each do |old_key, new_key|
         if !results['hits']['hits'].empty?
           set = []
           results["hits"]["hits"].to_a.each do |doc|
