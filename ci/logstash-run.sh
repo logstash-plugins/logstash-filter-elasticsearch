b1f591d15c1be0c8d471589cbc072be2def8600d
Rob Bavey <rob.bavey@elastic.co>
2019-10-22 15:24:47 -0400

    Remove more unneeded options

--- ci/logstash-run.sh
+++ ci/logstash-run.sh
@@ -6,9 +6,6 @@ export PATH=$BUILD_DIR/gradle/bin:$PATH
 wait_for_es() {
   echo "Waiting for elasticsearch to respond..."
   es_url="http://elasticsearch:9200"
-  if [[ "$SECURE_INTEGRATION" == "true" ]]; then
-    es_url="https://elasticsearch:9200 -k"
-  fi
   count=120
   while ! curl --silent $es_url && [[ $count -ne 0 ]]; do
     count=$(( $count - 1 ))
@@ -20,17 +17,10 @@ wait_for_es() {
   return 0
 }
 
-#if [[ "$INTEGRATION" != "true" ]]; then
-#  bundle exec rspec -fd spec/filters -t ~integration -t ~secure_integration
-#else
-#
-#  if [[ "$SECURE_INTEGRATION" == "true" ]]; then
-#    extra_tag_args="--tag secure_integration"
-#  else
-    extra_tag_args="--tag ~secure_integration --tag integration"
-#  fi
-
+if [[ "$INTEGRATION" != "true" ]]; then
+  bundle exec rspec -fd spec/filters -t ~integration
+else
+  extra_tag_args="--tag integration"
   wait_for_es
   bundle exec rspec -fd $extra_tag_args --tag es_version:$ELASTIC_STACK_VERSION spec/filters/integration
-#fi
-
+fi
