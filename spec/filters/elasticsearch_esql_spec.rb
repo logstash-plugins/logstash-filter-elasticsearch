fe5074cb4d618a40db18cacbaee76943c45b32ec
Mashhur <mashhur.sattorov@elastic.co>
2025-06-09 23:23:26 -0700

    Place the query results based on the target specified. If not specified, first result will be set to event's top level.

--- spec/filters/elasticsearch_esql_spec.rb
+++ spec/filters/elasticsearch_esql_spec.rb
@@ -17,10 +17,10 @@ describe LogStash::Filters::Elasticsearch::EsqlExecutor do
   context "when initializes" do
     it "sets up the ESQL executor with correct parameters" do
       allow(logger).to receive(:debug)
+      allow(logger).to receive(:warn)
       expect(esql_executor.instance_variable_get(:@query)).to eq(plugin_config["query"])
       expect(esql_executor.instance_variable_get(:@referenced_params)).to eq({})
       expect(esql_executor.instance_variable_get(:@static_params)).to eq([])
-      expect(esql_executor.instance_variable_get(:@fields)).to eq({})
       expect(esql_executor.instance_variable_get(:@tag_on_failure)).to eq(["_elasticsearch_lookup_failure"])
     end
   end
@@ -30,17 +30,21 @@ describe LogStash::Filters::Elasticsearch::EsqlExecutor do
       super()
         .merge(
           {
-            "query_type" => "esql",
             "query" => "FROM my-index | WHERE field = ?foo | LIMIT 5",
-            "query_params" => { "foo" => "[bar]" },
-            "fields" => { "val" => "val_new", "odd" => "new_odd" }
+            "query_params" => { "foo" => "[bar]" }
           })
     }
     let(:event) { LogStash::Event.new({}) }
-    let(:response) { { 'values' => [["foo", "bar", nil]], 'columns' => [{ 'name' => 'id' }, { 'name' => 'val' }, { 'name' => 'odd' }] } }
+    let(:response) {
+      {
+        'values' => [["foo", "bar", nil]],
+        'columns' => [{ 'name' => 'id', 'type' => 'keyword' }, { 'name' => 'val', 'type' => 'keyword' }, { 'name' => 'odd', 'type' => 'keyword' }]
+      }
+    }
 
     before do
       allow(logger).to receive(:debug)
+      allow(logger).to receive(:warn)
     end
 
     it "resolves parameters" do
@@ -66,7 +70,9 @@ describe LogStash::Filters::Elasticsearch::EsqlExecutor do
 
     it "processes the response and adds metadata" do
       expect(event).to receive(:set).with("[@metadata][total_values]", 1)
-      expect(event).to receive(:set).with("val_new", "bar")
+      # [id], [val] aren't resolved via sprintf, use as it is
+      expect(event).to receive(:set).with("[id]", "foo")
+      expect(event).to receive(:set).with("[val]", "bar")
       esql_executor.send(:process_response, event, response)
     end
 
@@ -85,8 +91,8 @@ describe LogStash::Filters::Elasticsearch::EsqlExecutor do
       expect { esql_executor.process(client, event) }.to_not raise_error
       expect(event.get("[@metadata][total_values]")).to eq(1)
       expect(event.get("hello")).to eq("world")
-      expect(event.get("val_new")).to eq("bar")
-      expect(event.get("new_odd")).to be_nil # filters out non-exist fields
+      expect(event.get("val")).to eq("bar")
+      expect(event.get("odd")).to be_nil # filters out non-exist fields
     end
 
     it "tags on plugin failures" do
@@ -105,11 +111,68 @@ describe LogStash::Filters::Elasticsearch::EsqlExecutor do
       expect(event).to receive(:tag).with("_elasticsearch_lookup_failure")
       esql_executor.process(client, event)
     end
+
+    describe "#target" do
+      let(:event) { LogStash::Event.new({ "hello" => "world", "bar" => "resolve_me" }) }
+      let(:response) {
+        super().merge({ 'values' => [["foo", "bar", nil], %w[hello again world], %w[another value here]] })
+      }
+      before(:each) do
+        expect(client).to receive(:esql_query).with(any_args).and_return(response)
+        allow(plugin).to receive(:decorate)
+        allow(logger).to receive(:debug)
+        allow(response).to receive(:headers).and_return({})
+      end
+
+      context "when specified" do
+        let(:plugin_config) {
+          super().merge({ "target" => "my-target" })
+        }
+
+        it "sets all query results into event" do
+          expected_result = [
+            {"id"=>"foo", "val"=>"bar", "odd"=>nil},
+            {"id"=>"hello", "val"=>"again", "odd"=>"world"},
+            {"id"=>"another", "val"=>"value", "odd"=>"here"}
+          ]
+          expect { esql_executor.process(client, event) }.to_not raise_error
+          expect(event.get("[@metadata][total_values]")).to eq(3)
+          expect(event.get("my-target").size).to eq(3)
+          expect(event.get("my-target")).to eq(expected_result)
+        end
+      end
+
+      context "when not specified" do
+        shared_examples "first result into the event" do
+          it "sets" do
+            expect { esql_executor.process(client, event) }.to_not raise_error
+            expect(event.get("[@metadata][total_values]")).to eq(3)
+            expect(event.get("id")).to eq("foo")
+            expect(event.get("val")).to eq("bar")
+            expect(event.get("odd")).to eq(nil)
+          end
+        end
+        context "when limit is included in the query" do
+          let(:plugin_config) {
+            super().merge({ "query" => "FROM my-index | LIMIT 555" })
+          }
+          it_behaves_like "first result into the event"
+        end
+
+        context "when limit is not included in the query" do
+          let(:plugin_config) {
+            super().merge({ "query" => "FROM my-index" })
+          }
+          it_behaves_like "first result into the event"
+        end
+      end
+    end
   end
 
   describe "#query placeholders" do
     before(:each) do
       allow(logger).to receive(:debug)
+      allow(logger).to receive(:warn)
       plugin.send(:validate_esql_query_and_params!)
     end
 
