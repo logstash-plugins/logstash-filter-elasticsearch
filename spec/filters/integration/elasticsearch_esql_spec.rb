2b445a22d4d472660d4d28450bc823a70228c39e
Mashhur <mashhur.sattorov@elastic.co>
2025-07-03 16:29:05 -0700

    Integration tests to run with credentials anebled and SSL configs.

--- spec/filters/integration/elasticsearch_esql_spec.rb
+++ spec/filters/integration/elasticsearch_esql_spec.rb
@@ -6,8 +6,10 @@ require_relative "../../../spec/es_helper"
 
 describe LogStash::Filters::Elasticsearch, integration: true do
 
+  ELASTIC_SECURITY_ENABLED = ENV['ELASTIC_SECURITY_ENABLED'].eql? 'true'
   SECURE_INTEGRATION = ENV['SECURE_INTEGRATION'].eql? 'true'
   ES_HOSTS = ["http#{SECURE_INTEGRATION ? 's' : nil}://#{ESHelper.get_host_port}"]
+  CA_PATH = File.expand_path('../fixtures/test_certs/ca.crt', File.dirname(__FILE__))
 
   let(:plugin) { described_class.new(config) }
   let(:es_index) { "es-filter-plugin-esql-integration-#{rand(1000)}" }
@@ -21,25 +23,49 @@ describe LogStash::Filters::Elasticsearch, integration: true do
       { "message" => "odd test message", "type" => "t" }
     ]
   end
-  let(:config) do
+
+  let(:base_config) do
     {
+      "query_type" => "esql",
       "hosts" => ES_HOSTS,
-      "query_type" => "esql"
+      "ssl_enabled" => SECURE_INTEGRATION
     }
   end
+
+  let(:credentials) do
+    if SECURE_INTEGRATION
+      { 'user' => 'tests', 'password' => 'Tests123' }
+    else
+      { 'user' => 'elastic', 'password' => ENV['ELASTIC_PASSWORD'] }
+    end
+  end
+
+  let(:config) do
+    config = ELASTIC_SECURITY_ENABLED ? base_config.merge(credentials) : base_config
+    config = { 'ssl_certificate_authorities' => CA_PATH }.merge(config) if SECURE_INTEGRATION
+    config
+  end
+
   let(:event) { LogStash::Event.new({}) }
-  let(:es_client) do
-    Elasticsearch::Client.new(hosts: ES_HOSTS)
+
+  def es_client
+    @es_client ||= begin
+      user = SECURE_INTEGRATION ? 'tests' : 'elastic'
+      password = SECURE_INTEGRATION ? 'Tests123' : ENV['ELASTIC_PASSWORD']
+
+      es_client_config = { hosts: ES_HOSTS }
+      es_client_config = es_client_config.merge({ user: user, password: password }) if ELASTIC_SECURITY_ENABLED || SECURE_INTEGRATION
+      es_client_config = es_client_config.merge({ transport_options: { ssl: { ca_path: CA_PATH, verify: false }}}) if SECURE_INTEGRATION
+
+      Elasticsearch::Client.new(es_client_config)
+    end
   end
 
   before(:all) do
     is_ls_with_esql_supported_client = Gem::Version.create(LOGSTASH_VERSION) >= Gem::Version.create(LogStash::Filters::Elasticsearch::LS_ESQL_SUPPORT_VERSION)
+    # Skip tests if an ES version doesn't support ES|QL
     skip "LS version does not have ES client which supports ES|QL" unless is_ls_with_esql_supported_client
 
-    # Skip tests if an ES version doesn't support ES||QL
-    es_client = SECURE_INTEGRATION ?
-                  Elasticsearch::Client.new(hosts: ES_HOSTS, user: 'tests', password: 'Tests123') :
-                  Elasticsearch::Client.new(hosts: ES_HOSTS)
     es_version_info = es_client.info["version"]
     es_gem_version = Gem::Version.create(es_version_info["number"])
     skip "ES version does not support ES|QL" if es_gem_version.nil? || es_gem_version < Gem::Version.create(LogStash::Filters::Elasticsearch::ES_ESQL_SUPPORT_VERSION)
